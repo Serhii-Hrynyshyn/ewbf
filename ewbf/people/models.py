853ef2eaaeae5efd9409773a69b4ca7277e24757
Hrynyshyn Serhii <hrynyshyn.serhii@gmail.com>
2026-02-22 20:26:21 +0200

    create superuser

--- ewbf/people/models.py
+++ ewbf/people/models.py
@@ -8,13 +8,22 @@ class Person(models.Model):
     email = models.CharField("Е-мейл", max_length=100)
     address = models.CharField("Поточна адреса", max_length=250)
     registration_date = models.DateTimeField('Дата реєстрації')
+    login = models.CharField(max_length=50, blank=True, null=True)
+    parol = models.CharField(max_length=8, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Персона'
+        verbose_name_plural = 'Персони'
 
     def __str__(self):
         return f"""
+                    ПРОФАЙЛ КОРИСТУВАЧА
+        -----------------------------------------
         Ім'я: {self.name}
         Прізвище: {self.last_name}
         Телефонний номер: {self.phone_number}
         Електронна адресв: {self.email}
-        Поточне місце проживання: {self.address}
+        Поточне місце проживання: 
+        {self.address}
         Дата реєстрації: {self.registration_date}
         """
